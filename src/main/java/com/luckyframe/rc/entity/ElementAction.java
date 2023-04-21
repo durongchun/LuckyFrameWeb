170db9135995b8923ead39660c84b4f8b572ccc1
lucy <lucy@DESKTOP-PDBANR1>
2023-04-21 09:49:31 +0800

    update setting

--- src/main/java/com/luckyframe/rc/entity/ElementAction.java
+++ src/main/java/com/luckyframe/rc/entity/ElementAction.java
@@ -20,8 +20,39 @@ public class ElementAction {
     String access;
     String action;
     String actionValue;
+    
 
-    @Override
+    public String getAccess() {
+		return access;
+	}
+
+
+	public void setAccess(String access) {
+		this.access = access;
+	}
+
+
+	public String getAction() {
+		return action;
+	}
+
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+
+	public String getActionValue() {
+		return actionValue;
+	}
+
+
+	public void setActionValue(String actionValue) {
+		this.actionValue = actionValue;
+	}
+
+
+	@Override
     public String toString() {
         return "ElementAction{" +
                 "access='" + access + '\'' +
